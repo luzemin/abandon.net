5923a7d7f24a095a0873a611778e467b793288a1
Timothy Lu <timothy.lu@chinacbi.com>
2026-01-13 15:43:18 +0800

    add NPOI

---
+++ src/Abandon.NET/Models/SkyRecord.cs
@@ -0,0 +1,10 @@
+﻿namespace Abandon.NET.Models;
+
+public class SkyRecord
+{
+    public string SearchItemCode { get; set; }
+    public List<string> SkySubjectName { get; set; }
+    public List<string> Country { get; set; }
+    public List<string> ProductName { get; set; }
+    public List<string> Level { get; set; }
+}
